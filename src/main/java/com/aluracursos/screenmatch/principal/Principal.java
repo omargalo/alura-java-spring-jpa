02c51161209f230b374247ef5cd3ccd58d6f24a2
Omar García López <omar.garcia@omargl.net>
2024-05-15 16:01:45 -0600

    search by title

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -32,6 +32,7 @@ public class Principal {
                     1 - Buscar series 
                     2 - Buscar episodios
                     3 - Mostrar series buscadas
+                    4 - Buscar series por título
                                   
                     0 - Salir
                     """;
@@ -48,6 +49,9 @@ public class Principal {
                     break;
                 case 3:
                     mostrarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriesPorTitulo();
                     break;
 
                 case 0:
@@ -115,6 +119,19 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+    private void buscarSeriesPorTitulo() {
+        System.out.println("Escribe el nombre de la serie que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainsIgnoreCase(nombreSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("Serie buscada: " + serieBuscada.get());
+        } else {
+            System.out.println("Serie no encontrada");
+        }
+
     }
 
 
